f41a4082eb938c5a448d52159c53dc2a2f016b12
Logan Wright <logan.william.wright@gmail.com>
2016-02-16 00:26:46 -0500

    update to nep 2 protocol

--- Sources/Nest.swift
+++ Sources/Nest.swift
@@ -1,20 +1,28 @@
 /// Represents a HTTP Header, Key and Value
 public typealias Header = (String, String)
 
+
+/// Represents a HTTP request or response body
+public protocol PayloadType {
+    /// Returns the next byte in the payload
+    mutating func next() -> Int8?
+}
+
+
 /// Represents a HTTP Request
 public protocol RequestType {
-  var method: String { get }
-  var path: String { get }
-  var headers: [Header] { get }
-  var body: [UInt8]? { get }
+    var method:String { get }
+    var path:String { get }
+    var headers:[Header] { get }
+    var body: PayloadType? { get }
 }
 
 
 /// Represents a HTTP Response
 public protocol ResponseType {
-  var statusLine: String { get }
-  var headers: [Header] { get }
-  var body: [UInt8]? { get }
+    var statusLine:String { get }
+    var headers:[Header] { get }
+    var body: PayloadType? { get }
 }
 
 public typealias Application = RequestType -> ResponseType
